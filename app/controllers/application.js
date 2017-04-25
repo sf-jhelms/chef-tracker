581cb6ff4ee2fcfdacb792ba4b0d45eff12c3e5e
sf-jhelms <jeff.helms.gdpj@statefarm.com>
2017-04-25 15:34:21 -0500

    Chef Tracker prior to Challenges

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  chefsLength: Ember.computed.alias('model.length'),
+  availableChefs: Ember.computed.filterBy('model', 'isCooking', true),
   actions: {
     makeExit(chef){
     Ember.set(chef,'isCooking', false)
@@ -9,6 +11,16 @@ export default Ember.Controller.extend({
     makeEnter(chef){
     Ember.set(chef,'isCooking', true)
     chef.save();
+  },
+  saveNewItem(){
+    this.store.createRecord('chef', {
+      isCooking: false,
+      name: this.get('newItem')
+    }).save()
+    this.set('newItem', '')
+  },
+  destroyItem(chef){
+    chef.destroyRecord();
   }
   }
 });
