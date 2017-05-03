447a96b4eacb06cad4c243978a332e832ea1648a
sf-jhelms <jeff.helms.gdpj@statefarm.com>
2017-05-03 12:54:44 -0500

    Latest version with recent exercises

--- app/models/chef.js
+++ app/models/chef.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   isCooking: DS.attr('boolean'),
-  numStudents: DS.attr('number', {defaultValue: 0})
+  numStudents: DS.attr('number', {defaultValue: 0}),
+  description: DS.attr('string')
 });
